b1f86fe36385ec7fafbccb87a6aa4a53dcab127e
muhanguzi thomas <tommijxt15@gmail.com>
2026-02-28 02:02:11 +0300

    Reduce password reset throttle to 10 seconds

--- config/auth.php
+++ config/auth.php
@@ -95,7 +95,7 @@ return [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 5,
-            'throttle' => 60,
+            'throttle' => 10,
         ],
     ],
 
